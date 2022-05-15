471acb1ea53881d14b7c542a1658a0f3caea640c
florinERotaru <florineugen271@gmail.com>
2022-05-15 21:09:59 +0300

    almost done

--- LAB8/LAB8/LAB8.cpp
+++ LAB8/LAB8/LAB8.cpp
@@ -4,13 +4,74 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-int main() {
+#include <map>
+#include <queue>
+struct compare {
+    bool operator()(const std::pair<std::string, int>& p1, const std::pair<std::string, int>& p2) const{
+        return p1.second < p2.second;
+    }
+};
+void minimize(std::string &str) {
+    for (int i = 0; i < str.length(); i++)
+        str[i] = tolower(str[i]);
+}
+void print_map(std::map<std::string, int> map) {
+    std::cout << "===mapped phrase===\n";
+    std::map<std::string, int>::iterator it;
+    for (it = map.begin(); it != map.end(); it++)
+        std::cout << it->first << " : " << it->second << "\n";
+}
 
+void print_queue(
+      std::priority_queue<std::pair<std::string, int>, 
+    std::vector<std::pair<std::string, int>>, compare> my_queue) {
+    std::cout << "===priority queue===\n";
+    while (!my_queue.empty()) {
+        std::cout << my_queue.top().first << " => " << my_queue.top().second << "\n";
+        my_queue.pop();
+    }
+}
+void phrase_to_map(std::string s, std::map<std::string, int> &freq_map) {
+    std::string tok;
+    std::string delim = " ,.!?";
+    for (int i = 0; i < s.length(); i++) {
+        if (delim.find(s[i]) != std::string::npos) {
+            if (!tok.empty()) {
+                minimize(tok);
+                freq_map[tok]++;
+            }
+            tok.clear();
+            continue;
+        }
+        tok += s[i];
+    }
+}
+void map_to_queue(std::map<std::string, int> map, 
+            std::priority_queue < std::pair<std::string, int>,
+                std::vector<std::pair<std::string, int>>, 
+                    compare> &my_queue) {
+    std::map<std::string, int>::iterator it;
+    for (it = map.begin(); it != map.end(); it++) {
+        std::pair<std::string, int> my_pair;
+        my_pair.first = it->first;
+        my_pair.second = it->second;
+        my_queue.push(my_pair);
+    }
+
+}
+
+int main() {
     std::string s;
+    std::map<std::string, int> freq_map;
     std::fstream file_obj;
-    int it = 0;
     file_obj.open("test.txt", std::fstream::in | std::fstream::out | std::fstream::app);
     std::getline(file_obj, s);
-
+    phrase_to_map(s, freq_map);
+    print_map(freq_map);
+    std::priority_queue < std::pair<std::string, int>,
+                std::vector<std::pair<std::string, int>>, 
+                    compare> my_queue;
+    map_to_queue(freq_map, my_queue);
+    print_queue(my_queue);
     return 0;
 }
